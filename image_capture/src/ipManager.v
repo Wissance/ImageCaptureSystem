93b0cb78fd0c02917ed0bb945d4aa97ebec7f1e8
Unknown <um.nix.user@gmail.com>
2016-09-23 11:33:02 +0400

    Добавлены Verilog-модули с набором входов и выходов для реализации ядра видео захвата
    Добавлены Verilog-модули с набором входов и выходов для реализации ядра видео захвата

---
+++ image_capture/src/ipManager.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 23.09.2016 08:48:06
+// Design Name: 
+// Module Name: manager
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ipManager(
+    input start,
+    input stop,
+    input [31:0] configuration,
+   output lineCaptured,
+    input getData,
+    output [7:0] data
+    );
+endmodule
---
+++ image_capture/src/memoryManagementUnit.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 23.09.2016 09:44:53
+// Design Name: 
+// Module Name: memoryController
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module memoryManagementUnit(
+    input [7:0] data,
+    input clock,
+    output [7:0] out,
+    input enable
+    );
+endmodule
