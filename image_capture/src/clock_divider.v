6dfab93711a3911ef8e060315a01c2382d04208f
Unknown <um.nix.user@gmail.com>
2016-10-06 07:43:27 +0400

    Тестирование в поведенческом симуляторе (какая же вивадо кривая это просто жесть)
    Тестирование в поведенческом симуляторе (какая же вивадо кривая это просто жесть)

--- image_capture/src/clock_divider.v
+++ image_capture/src/clock_divider.v
@@ -21,15 +21,19 @@
 
 
 module clock_divider #
-    (
-        parameter clock_division = 2
-    )
-    (
-        input wire input_clock,
-        output reg output_clock
-    );
+(
+    parameter clock_division = 2
+)
+(
+    input wire input_clock,
+    output reg output_clock = 0
+); 
+    //reg[7:0] counter;
     
-    reg counter = 0;
+    //initial counter = 8'b00000000;
+    
+    integer counter = 0;
+
     
     if (clock_division == 1)
     begin            
@@ -38,15 +42,16 @@ module clock_divider #
             output_clock <= input_clock;
         end
     end
+    
     else
     begin
         always@ (posedge input_clock)
         begin
-            counter <= counter + 1;
+            counter = counter + 1;
             if(counter == clock_division)
             begin
-                output_clock <= ~output_clock;
-                counter <= 0;
+                output_clock = ~output_clock;
+                counter = 0;
             end
         end
     end
