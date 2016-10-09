f6a87c5cbb687eae6fefc2cedc6ea45519538d6c
Unknown <um.nix.user@gmail.com>
2016-10-10 00:36:04 +0400

    Модуль Fifo и тест на него (нужен для реализации собственного AXI Stream, вивадовский не работоспособен)
    Модуль Fifo и тест на него (нужен для реализации собственного AXI Stream, вивадовский не работоспособен)

---
+++ image_capture/image_capture.sim/linescanner2stream_convertor_tests/behav/elaborate.bat
@@ -0,0 +1,9 @@
+@echo off
+set xv_path=C:\\XilinxTools\\Vivado\\2016.2\\bin
+call %xv_path%/xelab  -wto 3f5aa38eb5794db5b10b89e0e066941c -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L xpm -L unisims_ver -L unimacro_ver -L secureip --snapshot linescanner2stream_convertor_testbench_behav xil_defaultlib.linescanner2stream_convertor_testbench xil_defaultlib.glbl -log elaborate.log
+if "%errorlevel%"=="0" goto SUCCESS
+if "%errorlevel%"=="1" goto END
+:END
+exit 1
+:SUCCESS
+exit 0
