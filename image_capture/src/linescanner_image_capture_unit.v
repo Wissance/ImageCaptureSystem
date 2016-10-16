a72fb71cebba082573731088a44034e6fa23c932
Alex Petrov <daotheman@gmail.com>
2016-10-16 06:41:21 +0500

    Добавлена еще одна SM, добавлены спец состояния для ожидания, блокируюшие присваивания заменены на неблокирующие

--- image_capture/src/linescanner_image_capture_unit.v
+++ image_capture/src/linescanner_image_capture_unit.v
@@ -39,99 +39,117 @@ module linescanner_image_capture_unit(
     assign main_clock = main_clock_source;
     assign pixel_captured = lval;
     assign pixel_data = data;
-    
-    reg[7:0] clock_counter, clock_count_end_adc_re, sm_state;
-    reg send_load_pulse;
+      
+    reg[7:0] sm1_state, sm2_state,
+    sm1_clock_count, sm2_clock_count,
+    sm1_num_clocks_to_wait, sm2_num_clocks_to_wait,
+    sm1_state_to_go_to_after_waiting, sm2_state_to_go_to_after_waiting;
     
     always @ (posedge pixel_clock) begin
         if(!n_reset) begin
-            clock_counter = 0;
-            sm_state = 0;
-            rst_cvc = 1'b1;
-            rst_cds = 1'b1;
-            sample = 1'b0;
+            sm1_clock_count <= 0;
+            sm1_state <= 0;
+            rst_cvc <= 1'b1;
+            rst_cds <= 1'b1;
+            sample <= 1'b0;
         end
         
-        else begin
-          case (sm_state)
-            0: begin
+        else
+            case (sm1_state)
+                0:
                 if(enable) begin
-                rst_cvc = 1'b0;
-                sm_state = 1;
+                    rst_cvc <= 1'b0;
+                    sm1_state <= 5;
+                        
+                    sm1_num_clocks_to_wait <= 48;
+                    sm1_state_to_go_to_after_waiting <= 1;
                 end
-            end
-            
-            1: if(clock_counter < 49) // 50 - 1
-                clock_counter = clock_counter + 1;
-              else begin
-                rst_cds = 1'b0;
-                clock_counter = 0;
-                sm_state = 2;
-              end
-              
-            2: if(clock_counter < 8) // skip 8clk
-                clock_counter = clock_counter + 1;
-              else if(end_adc) begin
-                sample = 1'b1;
-                clock_counter = 0;
-                sm_state = 3;
-               end
-               
-            3: if(clock_counter < 49) // 50 - 1
-                clock_counter = clock_counter + 1;
-              else begin
-                sample = 1'b0;
-                clock_counter = 0;
-                sm_state = 4;
-              end
-              
-            4: if(clock_counter < 6) // skip 7clk (7 - 1)
-                clock_counter = clock_counter + 1;
-                else begin
-                    clock_counter = 0;
-                    sm_state = 5;
-                end 
                 
-            5: begin
-                rst_cvc = 1'b1;
-                rst_cds = 1'b1;
-                sm_state = 6;
-            end
-            
-            6: if(clock_counter < 48) // 50 - 2
-                clock_counter = clock_counter + 1;
-              else begin
-                clock_counter = 0;
-                sm_state = 0;
-              end
-          endcase
-      end
+                1:
+                begin
+                    rst_cds <= 1'b0;
+                    sm1_clock_count <= 0;
+                    sm1_state <= 5;
+                    
+                    sm1_num_clocks_to_wait <= 7;
+                    sm1_state_to_go_to_after_waiting <= 2;
+                end
+                  
+                2:
+                begin
+                    sample <= 1'b1;
+                    sm1_clock_count <= 0;
+                    sm1_state <= 5;
+                    
+                    sm1_num_clocks_to_wait <= 48;
+                    sm1_state_to_go_to_after_waiting <= 3;
+                end
+                   
+                3:
+                begin
+                    sample <= 1'b0;
+                    sm1_clock_count <= 0;
+                    sm1_state <= 5;
+                    
+                    sm1_num_clocks_to_wait <= 6;
+                    sm1_state_to_go_to_after_waiting <= 4;
+                end
+                
+                4:
+                begin
+                    rst_cvc <= 1'b1;
+                    rst_cds <= 1'b1;
+                    sm1_state <= 0;
+                end
+                
+                5:
+                if(sm1_clock_count < sm1_num_clocks_to_wait)
+                    sm1_clock_count <= sm1_clock_count + 1;
+                else begin
+                    sm1_clock_count <= 0;
+                    sm1_state <= sm1_state_to_go_to_after_waiting;
+                end
+            endcase
     end
 
     always @ (posedge pixel_clock) begin
       if(!n_reset) begin
-        send_load_pulse = 1'b1;
-        clock_count_end_adc_re = 0;
-        load_pulse = 1'b0;
+        sm2_clock_count <= 0;
+        sm2_state <= 0;
+        load_pulse <= 1'b0;
       end
       
-      else begin      
-        if(end_adc) begin
-            if(load_pulse)
-                load_pulse = 1'b0;
-        
-            if(send_load_pulse) begin
-                if(clock_count_end_adc_re < 5) // 4 + 1
-                    clock_count_end_adc_re = clock_count_end_adc_re + 1;
-                else begin
-                    load_pulse = 1'b1;
-                    send_load_pulse = 1'b0;
-                    clock_count_end_adc_re = 0;
-                end
+      else
+        case(sm2_state)
+            0:
+            if(end_adc) begin
+                sm2_state <= 4;
+                sm2_state_to_go_to_after_waiting <= 1;
             end
-        end
-        else
-            send_load_pulse = 1'b1;
-      end
+            
+            1:
+            begin
+                load_pulse <= 1'b1;
+                sm2_state <= 2;
+            end
+            
+            2:
+            begin
+                load_pulse <= 1'b0;
+                sm2_state <= 3;
+            end
+            
+            3:
+            if(!end_adc)
+                sm2_state <= 0;
+            
+            4:
+            if(sm2_clock_count < 3)
+                sm2_clock_count <= sm2_clock_count + 1;
+            else begin
+                sm2_clock_count <= 0;
+                sm2_state <= sm2_state_to_go_to_after_waiting;
+            end
+        endcase
     end
 endmodule
