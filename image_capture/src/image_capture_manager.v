37804d90f7437cc84ed5a56afd34fd673e065f8a
Alex Petrov <daotheman@gmail.com>
2016-12-21 01:44:37 +0500

    Revert "Merge branch 'master'"
    This reverts commit b8029a932de912221f3b71aee80e2bcec0b2f7a5.

--- image_capture/src/image_capture_manager.v
+++ image_capture/src/image_capture_manager.v
@@ -76,9 +76,9 @@
 
 	// Add user logic here
 	// 1. Manage enable and clear signals
-	always @(posedge s00_axi_wstrb[0], posedge s00_axi_wvalid, posedge s00_axi_aclk)
+	always @(posedge s00_axi_wstrb[0], negedge s00_axi_awvalid)
 	begin
-	    if (~s00_axi_wvalid)
+	    if (~s00_axi_awvalid)
 	    begin
             clear_memory <= 0;
         end
