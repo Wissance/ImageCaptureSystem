2657a09af2021650742f5107799d138b13881ff8
Unknown <um.nix.user@gmail.com>
2017-01-26 10:07:43 +0400

    Настройки для успешного битстрима

--- image_capture/image_capture.srcs/sources_1/bd/image_capture_design/hdl/image_capture_design_wrapper.v
+++ image_capture/image_capture.srcs/sources_1/bd/image_capture_design/hdl/image_capture_design_wrapper.v
@@ -1,8 +1,8 @@
 //Copyright 1986-2016 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2016.2 (win64) Build 1577090 Thu Jun  2 16:32:40 MDT 2016
-//Date        : Thu Jan 12 14:08:47 2017
-//Host        : otheman-laptop running 64-bit major release  (build 9200)
+//Date        : Thu Jan 26 09:44:25 2017
+//Host        : XilinxDev running 64-bit Service Pack 1  (build 7601)
 //Command     : generate_target image_capture_design_wrapper.bd
 //Design      : image_capture_design_wrapper
 //Purpose     : IP block netlist
--- image_capture/src/linescanner2stream_convertor.v
+++ image_capture/src/linescanner2stream_convertor.v
@@ -1,4 +1,4 @@
-`timescale 1ns / 1ps
+//`timescale 1ns / 1ps
 //`include ".\src\linescanner2stream_convertor_M00_AXIS.v"
 `define PIXELS_BUFFER_SIZE 4
 `define BYTE_SIZE 8
@@ -51,9 +51,66 @@ module linescanner2stream_convertor #
      output wire  m00_axis_tlast,
      input wire  m00_axis_tready
 );
+    wire data_ready;
+    //wire[2:0] counter_output;
+    reg [1:0] byte_number;
+    //reg carry;
+    reg[C_M00_AXIS_TDATA_WIDTH - 1 : 0] output_data;
+    supply1 vcc;
+    supply0 gnd;
+
+    FDCE data_ready_trigger(.C(~byte_number[1]), .CE(enable), .CLR(data_ready), .D(vcc), .Q(data_ready));
+
+    //assign data_ready = carry;
+
+    //COUNTER_TC_MACRO #(.COUNT_BY(48'h00000001), .TC_VALUE(48'h00000004), .DEVICE("7SERIES"), .DIRECTION("UP"), .RESET_UPON_TC("FALSE"), .WIDTH_DATA(3))
+    //pixel_captured_counter (.Q(), .TC(data_ready), .CLK(pixel_captured), .CE(vcc), .RST(gnd));
+    
+    // Instantiation of Axi Bus Interface M00_AXIS
+    linescanner2stream_convertor_M00_AXIS # (.C_M_AXIS_TDATA_WIDTH(C_M00_AXIS_TDATA_WIDTH), .C_M_START_COUNT(C_M00_AXIS_START_COUNT)) 
+        linescanner2stream_convertor_M00_AXIS_inst (
+            .DATA_SOURCE(output_data),
+            .DATA_READY(data_ready),
+            .M_AXIS_ACLK(m00_axis_aclk),
+            .M_AXIS_ARESETN(m00_axis_aresetn),
+            .M_AXIS_TVALID(m00_axis_tvalid),
+            .M_AXIS_TDATA(m00_axis_tdata),
+            .M_AXIS_TSTRB(m00_axis_tstrb),
+            .M_AXIS_TLAST(m00_axis_tlast),
+            .M_AXIS_TREADY(m00_axis_tready)
+        );   
+    
+    always@ (negedge pixel_captured)
+    begin
+        if(!m00_axis_aresetn)
+        begin
+            output_data = 0;
+            byte_number = 0;
+        end
+        else
+        begin
+            if(enable)
+            begin
+                output_data = pack_pixel_data(byte_number, input_data);
+                byte_number = byte_number + 1;
+            end
+        end
+    end
+    
+/*    always@(posedge m00_axis_aclk)
+    begin
+        if(!m00_axis_aresetn)
+            carry <= 0;
+        else
+        begin
+            if(byte_number == 0)
+                carry <= 1;
+            else carry <= 0;
+        end
+    end*/
 
 	// Add user logic here
-	wire data_ready_net;
+	/*wire data_ready_net;
 	reg log1_rised = 0;
 	reg data_ready_value = 0;
 	reg[7:0] pixel_counter = 0;
@@ -118,7 +175,7 @@ module linescanner2stream_convertor #
                end
            end
         end
-	end
+	end*/
 	
 	function [31:0] pack_pixel_data;
 	
