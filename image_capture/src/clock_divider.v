e8c2b853b78065af0ac46845ea10a02ad00baed3
Unknown <um.nix.user@gmail.com>
2017-04-20 11:38:49 +0500

    Очистка от комментариев

--- image_capture/src/clock_divider.v
+++ image_capture/src/clock_divider.v
@@ -1,20 +1,20 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: EasySoft Group
+// Engineer: Ushakov M.V. (EvilLord666)
 // 
 // Create Date: 04.10.2016 05:53:55
 // Design Name: 
 // Module Name: clock_divider
 // Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
+// Target Devices: Zynq-7k
+// Tool Versions: Vivado 2016.2
+// Description: Module for programmable clock division from 1 till 255
 // 
 // Dependencies: 
 // 
-// Revision:
-// Revision 0.01 - File Created
+// Revision: 
+// Revision 1.0
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
@@ -33,32 +33,7 @@ module clock_divider #
     reg output_clock_value;
     
     assign output_clock =  clock_division == 1 ? input_clock & reset : output_clock_value; 
-    
-    //initial counter = 8'b00000000;
-    
-/*    integer counter = 0;
 
-    
-    if (clock_division == 1)
-    begin            
-        always@ (input_clock)
-        begin
-            output_clock <= input_clock;
-        end
-    end
-    
-    else
-    begin
-        always@ (posedge input_clock)
-        begin
-            counter = counter + 1;
-            if(counter == clock_division)
-            begin
-                output_clock = ~output_clock;
-                counter = 0;
-            end
-        end
-    end*/
     
     always @(posedge input_clock)
     begin
@@ -76,8 +51,6 @@ module clock_divider #
              end
              else
                  counter <= counter + 1;
-         //if(counter == clock_division - 1)
-             //counter <= 0;
          end
     end
     
